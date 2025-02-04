1401327972eb2b368aebd665f644c9956f5c4fbe
Chaejin Lim <jin.chaejin.lim@adcapsule.co.kr>
2025-02-04 14:41:53 +0900

    WIP

--- core/src/main/java/com/adcapsule/server52switch/core/configs/MongoConfig.java
+++ core/src/main/java/com/adcapsule/server52switch/core/configs/MongoConfig.java
@@ -28,6 +28,6 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         // Create the MongoClient with the URI from the .env file
         //return MongoClients.create(dotenv.get("MONGODB_URI"));
         
-        return MongoClients.create(System.getenv("MONGODB_URI")+"retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true/");
+        return MongoClients.create(System.getenv("MONGODB_URI")+getDatabaseName()+"?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true");
     }
 }
